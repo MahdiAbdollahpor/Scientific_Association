b1d4c1ba516b53de20047e3bd2c1837969bc0abd
mahdi <madi1381abdollahpor@gmail.com>
2025-04-07 13:30:45 +0400

    init-1

---
+++ DataLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Models.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        
+
+    }
+}
--- DataLayer/Models/Identity/User.cs
+++ DataLayer/Models/Identity/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,7 +14,20 @@ namespace DataLayer.Models.Identity
         public int UserId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string nationalCode { get; set; }
+        public string studentNumber { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterTime { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
 
+
+        #region rel
+
+        public IEnumerable<UserRole> UserRoles { get; set; }
+
+
+        #endregion
 
     }
 }
