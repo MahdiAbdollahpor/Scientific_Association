e90acd7078e842d7b7fccca48df941e73853a8f0
mahdi <madi1381abdollahpor@gmail.com>
2025-04-07 13:14:15 +0400

    Add project files.

---
+++ DataLayer/Models/Identity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models.Identity
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+
+    }
+}
